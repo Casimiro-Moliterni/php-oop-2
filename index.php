f663c670a8a2bb7f8b5270a1c0021beae96646cb
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-05-15 15:15:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!-- TRACCIA 
+Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+- L'e-commerce vende prodotti per animali.
+- I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+- I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+
+Stampiamo delle card contenenti i dettagli dei prodotti,
+ come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando
+  (prodotto, cibo, gioco, cuccia). -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pet Shop</title>
+    <!-- link bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+        <section>
+    
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
